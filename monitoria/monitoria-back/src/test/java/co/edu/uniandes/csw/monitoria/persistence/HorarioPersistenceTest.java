9bfa150c5dcf039a9a4bc5b9ef08aa1859ea56ce
Cristian Cardenas <cc.cardenas@uniandes.edu.co>
2017-09-12 12:05:53 -0500

    Persistence Tests de ESTUDIANTE Y HORARIO v2

--- monitoria/monitoria-back/src/test/java/co/edu/uniandes/csw/monitoria/persistence/HorarioPersistenceTest.java
+++ monitoria/monitoria-back/src/test/java/co/edu/uniandes/csw/monitoria/persistence/HorarioPersistenceTest.java
@@ -13,6 +13,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
@@ -22,6 +23,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -29,6 +31,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  *
  * @author Cristian
  */
+@RunWith(Arquillian.class)
 public class HorarioPersistenceTest {
     
     public HorarioPersistenceTest() {
