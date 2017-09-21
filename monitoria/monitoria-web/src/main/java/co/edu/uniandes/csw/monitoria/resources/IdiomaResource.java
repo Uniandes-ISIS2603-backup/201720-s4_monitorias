4136741099cedcee3b8e66c57bb8e3fcffa97d7c
carlosmendoza <30907555+carlosmendoza@users.noreply.github.com>
2017-09-21 13:32:29 -0500

    Update IdiomaResource.java

--- monitoria/monitoria-web/src/main/java/co/edu/uniandes/csw/monitoria/resources/IdiomaResource.java
+++ monitoria/monitoria-web/src/main/java/co/edu/uniandes/csw/monitoria/resources/IdiomaResource.java
@@ -56,7 +56,7 @@ public class IdiomaResource {
     }
     
     @POST
-    public IdiomaDTO createCity(IdiomaDTO idioma) throws BusinessLogicException 
+    public IdiomaDTO createIdioma(IdiomaDTO idioma) throws BusinessLogicException 
     {
         IdiomaEntity IdiomaEntity = idioma.toEntity();
         IdiomaEntity nuevoIdioma = idiomaLogic.createIdioma(IdiomaEntity);
