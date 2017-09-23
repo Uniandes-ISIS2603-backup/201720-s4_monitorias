21c5a06c167b33d7ae11e2dd1964af5001e003fa
CARLOS ALBERTO MENDOZA PATALAGUA <ca.mendoza@uniandes.edu.co>
2017-09-23 11:52:46 -0500

    Se modifico findByName y se borraron imports innecesarios

--- monitoria/monitoria-back/src/main/java/co/edu/uniandes/csw/monitoria/persistence/IdiomaPersistence.java
+++ monitoria/monitoria-back/src/main/java/co/edu/uniandes/csw/monitoria/persistence/IdiomaPersistence.java
@@ -4,26 +4,10 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.monitoria.persistence;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import co.edu.uniandes.csw.monitoria.entities.IdiomaEntity;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 /**
  *
  * @author ca.mendoza
  */
-
-import co.edu.uniandes.csw.monitoria.ejb.IdiomaLogic;
-import co.edu.uniandes.csw.monitoria.entities.BibliotecaEntity;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -31,8 +15,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import co.edu.uniandes.csw.monitoria.entities.IdiomaEntity;
-import java.util.Date;
-import java.util.logging.Level;
 
 @Stateless
 
@@ -67,8 +49,8 @@ import java.util.logging.Level;
     }
   
     public IdiomaEntity findByName(String name){
-        TypedQuery query = em.createQuery("select e From IdiomaEntity e where e.nombre = :name", IdiomaEntity.class);
-        query = query.setParameter("name",name);
+        TypedQuery query = em.createQuery("select e From IdiomaEntity e where e.idioma = :name", IdiomaEntity.class);
+        query = query.setParameter("idioma",name);
         
         List<IdiomaEntity> sameName = query.getResultList();
         if(sameName.isEmpty()){
