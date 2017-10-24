cab83476ff53cce52805220f3a7ab1d65e206d2a
CARLOS ALBERTO MENDOZA PATALAGUA <ca.mendoza@sis.virtual.uniandes.edu.co>
2017-10-24 06:59:21 -0500

    Cambios en actividades

---
+++ monitoria-web/src/main/webapp/src/modules/actividades/delete/actividades.delete.ctrl.js
@@ -0,0 +1,16 @@
+(function (ng) {
+    var mod = ng.module("IdiomasModule");
+    mod.constant("idiomasContext", "api/idiomas");
+    mod.controller('idiomaDeleteCtrl', ['$scope', '$http', 'idiomasContext', '$state',
+        
+        function ($scope, $http, idiomasContext, $state) {
+            var idIdioma = $state.params.idiomaId;
+            $scope.deleteIdioma = function () {
+                $http.delete(idiomasContext + '/' + idIdioma, {}).then(function (response) {
+                    $state.go('idiomasList', {idiomaId: response.data.id}, {reload: true});
+                });
+            };
+        }
+    ]);
+}
+)(angular);
