94eb1c88e99fd27f5e528e88738e4d5bca08d9a5
MichaelOsorio2017 <ms.osorio@uniandes.edu.co>
2017-11-01 10:42:03 -0500

    smell arreglado (?)

--- monitoria-back/src/main/java/co/edu/uniandes/csw/monitoria/ejb/RecursoLogic.java
+++ monitoria-back/src/main/java/co/edu/uniandes/csw/monitoria/ejb/RecursoLogic.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.uniandes.csw.monitoria.ejb;
 
 import co.edu.uniandes.csw.monitoria.entities.BibliotecaEntity;
@@ -52,18 +47,12 @@ public class RecursoLogic {
         }else if((name.trim()).equals("")){
             throw new BusinessLogicException(ERROR);
         }
-        
-      
-        else
-        {
         BibliotecaEntity biblioteca = bibliotecaLogic.getBiblioteca(bibliotecaId);
         IdiomaEntity idioma = idiomaLogic.findByName(entity.getIdioma());
-       
         entity.setBiblioteca(biblioteca);
         entity.setDisponibilidad(Boolean.TRUE);
         entity.setIdioma(idioma);
         return persistence.createRecurso(entity);
-        }
     }
     
     /**
