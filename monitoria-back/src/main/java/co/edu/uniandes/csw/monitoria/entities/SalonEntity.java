c99efcf5e1332662ee029c3aec55f973c1220634
s.guzman <s.guzman@ISIS2603S4-0046.sis.virtual.uniandes.edu.co>
2017-11-11 14:43:06 -0500

    Reparacion de relaciones

--- monitoria-back/src/main/java/co/edu/uniandes/csw/monitoria/entities/SalonEntity.java
+++ monitoria-back/src/main/java/co/edu/uniandes/csw/monitoria/entities/SalonEntity.java
@@ -1,4 +1,4 @@
- /*
+  /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -24,15 +24,20 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Entity
 public class SalonEntity implements Serializable
 {
-    /**
-     * Atributo que hace referencia a la disponibilidad de un salon
-     */
-    private boolean disponibilidad;
+
+    
     
     /**
+     * Atributo que hace referencia al identificador del salon
+     */
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    
+        /**
      * Atributo que hace referencia a la localizacion de un salon
      */   
-    private String localizacion;
+    private String nombre;
     
     /**
      * Atributo que hace referencia a la sede del salon
@@ -45,22 +50,10 @@ public class SalonEntity implements Serializable
      * Atributo que hace referencia a  los horarios del salon que ya se usan para dar monitorias
      */
     @PodamExclude
-    @OneToMany (mappedBy = "salon")
-    private List<HorarioEntity> horariosMonitoria;
+    @OneToMany
+    private List<HorarioEntity> horarios;
 
-    /**
-     * Atributo que hace referencia a los horarios libres que tiene el salon para que se den monitorias
-     */
-   // @PodamExclude
-    //@OneToMany (mappedBy = "salon1")
-    //private List<HorarioEntity> horariosAtencion;
-    
-    /**
-     * Atributo que hace referencia al identificador del salon
-     */
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id
-    private Long id;
+       
         /**
      * Metodo para recibir el id del salon
      * @return el id del salon 
@@ -77,38 +70,23 @@ public class SalonEntity implements Serializable
     {
         this.id = id;
     }
-      /**
-      * metodo para editar la disponibiidad del salon
-     * @param pDisponibilidad nueva disponibilidad
-      */   
-    public void setDisponibilidad(Boolean pDisponibilidad)
-    {
-        this.disponibilidad = pDisponibilidad;
-    }
-         /**
-     * Metodo para recibir la disponibilidad del salon
-     * @return la disponibilidad  del salon 
-     */   
-    public boolean isDisponibilidad ()
-    {
-        return disponibilidad;
-    }
+
     
       /**
       * metodo para editar la localizacion del salon
      * @param newLoc
       */   
-    public void setLocalizacion (String newLoc)
+    public void setNombre (String newLoc)
     {
-        this.localizacion = newLoc;
+        this.nombre = newLoc;
     }
          /**
      * Metodo para recibir la localizacion del salon
      * @return lalocalizacion del salon 
      */   
-    public String getLocalizacion ()
+    public String getNombre ()
     {
-        return localizacion;
+        return nombre;
     }
        /**
       * metodo para editar la sede del salon
@@ -132,17 +110,17 @@ public class SalonEntity implements Serializable
      * @param pHorarios
       */   
   
-    public void setHorariosMonitoria (List<HorarioEntity> pHorarios)
+    public void setHorarios (List<HorarioEntity> pHorarios)
     {
-       this.horariosMonitoria = pHorarios;
+       this.horarios = pHorarios;
    }
         /**
      * Metodo para recibir la lista de horarios de monitoria del salon
      * @return la lista de horarios de monitoria del salon 
      */    
-   public List<HorarioEntity> getHorariosMonitoria ()
+   public List<HorarioEntity> getHorarios ()
    {
-       return horariosMonitoria; 
+       return horarios; 
    }    
  
    
