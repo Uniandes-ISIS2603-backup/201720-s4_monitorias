a892d9b3617e712588b5912f7dfb97f8991c4573
cc.cardenas <cc.cardenas@ISIS2603S4-0019.sis.virtual.uniandes.edu.co>
2017-11-28 18:53:15 -0500

    Errores horario arreglados

--- monitoria-web/src/main/webapp/src/modules/horarios/create/horarios.create.ctrl.js
+++ monitoria-web/src/main/webapp/src/modules/horarios/create/horarios.create.ctrl.js
@@ -5,18 +5,20 @@
     mod.constant("horariosContex","api/horarios");
     
     mod.controller("horarioCreateCtrl", ['$scope', '$state', '$stateParams', '$http', 'horariosContext', function ($scope, $state, $stateParams, $http, horariosContext) {
-               
+            
                  $scope.createHorario = function(){
                    $http.post(horariosContext,
                            {
                                 horaInicio: $scope.horarioHoraInicio,
                                 horaFin: $scope.horarioHoraFin,
-                                estado:$scope.horarioEstado                               
+                                disponibilidad:$scope.horarioEstado  
+                               
                             }).then(function (response){
                                
                                 $state.go('horariosList',{horarioId: response.data.id},{reload: true});
                             });
                }
+                 console.log($scope.horarioHoraInicio);
         }
     ]);
 }
